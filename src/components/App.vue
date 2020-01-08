c1a667844016974e678f579a5f88ad433b60eeee
Eduardo Barros <joseeduardobarros@gmail.com>
2020-01-08 05:59:20 -0300

    Init dev MoviesList

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,12 +1,6 @@
 <template>
   <div>
     <h1>Lista de Filmes</h1>
-    <p>
-      <router-link to="/foo">Go to Foo</router-link>
-      <router-link to="/bar">Go to Bar</router-link>
-    </p>
-
-    <router-view></router-view>
   </div>
 </template>
 
@@ -14,18 +8,6 @@
 import { mapActions, mapState } from "vuex";
 import firebase from "../firebase";
 
-export default {
-  computed: {
-    username() {
-      return this.$route.params.username;
-    },
-  },
-  methods: {
-    goBack() {
-      window.history.length > 1 ? this.$router.go(-1) : this.$router.push("/");
-    },
-  },
-};
+export default {};
 </script>
-
 <style></style>
