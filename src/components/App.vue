c08c2b46e59454f752fc9cf723a141d6ec2149ac
Eduardo Barros <joseeduardobarros@gmail.com>
2020-01-08 03:22:04 -0300

    Finish Vuexfire tests

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
-    <h1>Hello World</h1>
+    <h1>Lista de Filmes</h1>
     <ul>
       <li v-for="todo in todos" :key="todo['.key']">
         {{ todo.key }} {{ todo }}
         <button @click="removeTodo(todo)">X</button>
       </li>
     </ul>
-    <button>Bind!</button>
+    <form @submit.prevent="addTodo">
+      <input type="text" v-model="newTodo" />
+      <button>Add #{{todos.length}}</button>
+    </form>
     <p></p>
   </div>
 </template>
@@ -20,15 +23,26 @@ const db = firebase.database();
 const todosRef = db.ref("todos");
 
 export default {
-  methods: {},
+  data: function() {
+    return {
+      newTodo: ""
+    };
+  },
   computed: mapState(["todos"]),
   created: function() {
     this.source = todosRef;
     this.$store.dispatch("bindTodosRef");
   },
   methods: {
+    addTodo: function() {
+      if (this.newTodo.trim()) {
+        this.source.push({
+          text: this.newTodo
+        });
+        this.newTodo = "";
+      }
+    },
     removeTodo: function(todo) {
-      console.log(todo);
       this.source.child(todo[".key"]).set(null);
     }
   }
